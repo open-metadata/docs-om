a5022ce18b0deed371dc1746922e58d0c76964b1
Ayush Shah <ayush@getcollate.io>
2026-05-22 18:08:00 +0530

    Docs: Narrow CodeLayout styling changes

--- snippets/components/CodeLayout/CodeLayout.jsx
+++ snippets/components/CodeLayout/CodeLayout.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import './CodeLayout.css';
 
-export const CodeLayout = ({ title, description, children }) => {
-  return (
-    <div className="code-layout">
-      <div>
-        {title && (
-          <h2>{title}</h2>
-        )}
-        {description && (
-          <div>
-            {description}
-          </div>
-        )}
-      </div>
-
-      <div className="code-layout-sticky">
-        {children}
-      </div>
+export const CodeLayout = ({ title, description, children }) => (
+  <div className="code-layout">
+    <div className="code-layout-content">
+      {title && <h4>{title}</h4>}
+      {description}
     </div>
-  );
-};
+    <div className="code-layout-code">{children}</div>
+  </div>
+);
